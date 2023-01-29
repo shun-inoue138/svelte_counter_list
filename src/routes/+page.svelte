eb55959765dab1b8ccbe21e40c1a3ec4d8ccfa5c
inoue <ht8sk13@icloud.com>
2023-01-29 23:06:17 +0900

    変数totalNumの変更時に画面に表示されたtotalNumが点滅するtransitionを付与

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,24 @@
 <script lang="ts">
 	import { counters } from '../store.js';
 	import Counter from '../components/Counter.svelte';
+	import { fade } from 'svelte/transition';
 	const addCounter = () => {
 		$counters = [...$counters, { title: 'new title', count: 0 }];
 	};
+	$: totalNum = $counters.reduce((acc, cur) => acc + cur.count, 0);
 </script>
 
 <main class="min-h-screen bg-gray-100">
 	<div class="w-[96%] mx-auto text-center">
 		<h1 class="text-4xl font-bold text-gray-900">Multiple Counter</h1>
+
 		<div class="text-3xl">
-			total: <span class="border-b border-black"
-				>{$counters.reduce((acc, cur) => acc + cur.count, 0)}</span
-			>
+			total:
+			{#key totalNum}
+				<span class="border-b border-black" in:fade>{totalNum}</span>
+			{/key}
 		</div>
+
 		<ul class="flex flex-wrap gap-1 ">
 			<!-- todo:TitleListコンポーネント化 -->
 			{#each $counters as counter}
