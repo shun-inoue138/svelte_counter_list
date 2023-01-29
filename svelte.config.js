9f3a65a8c2f1c46b5bd4e112e8fc3b6d4d022fae
inoue <ht8sk13@icloud.com>
2023-01-29 09:22:26 +0900

    Revert "一部修正"
    This reverts commit 4730da75985059513ccfa6f7dfd66d5c0c720e9a.

--- svelte.config.js
+++ svelte.config.js
@@ -4,9 +4,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			fallback: 'index.html'
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
