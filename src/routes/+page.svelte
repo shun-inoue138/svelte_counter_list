87739681d1eae773dec2c044adec9cdf31422321
inoue <ht8sk13@icloud.com>
2023-01-30 01:34:07 +0900

    スタイリング調整。TitleListとtotalNumとnew counterボタンをstickyに

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -12,32 +12,34 @@
 </script>
 
 <main class="min-h-screen bg-gray-100">
-	<div class="w-[96%] mx-auto text-center">
-		<h1 class="text-4xl font-bold text-gray-900">Multiple Counter</h1>
+	<div class="w-[96%] mx-auto text-center py-12">
+		<h1 class="text-6xl font-bold text-gray-900">Multiple Counters</h1>
 
-		<div class="text-3xl">
-			total:
-			{#key totalNum}
-				<span class="border-b border-black" in:fade>{totalNum}</span>
-			{/key}
+		<div class="sticky top-5">
+			<div class="text-3xl mb-8">
+				Total:
+				{#key totalNum}
+					<span in:fade>{totalNum}</span>
+				{/key}
+			</div>
+			<button on:click={addCounter} class="bg-blue-500 text-white px-8 py-2 rounded-3xl mb-8 "
+				>new counter</button
+			>
+			<div class="mb-8">
+				<TitleList
+					on:focus={(e) => {
+						clickedIndex = e.detail.index;
+					}}
+				/>
+			</div>
 		</div>
-		<button on:click={addCounter}>new counter</button>
-		<TitleList
-			on:focus={(e) => {
-				clickedIndex = e.detail.index;
-			}}
-		/>
 
 		<ul>
-			{#if $counters === null}
-				<li>no counter</li>
-			{:else}
-				{#each $counters as counter, index}
-					<li>
-						<Counter {index} {clickedIndex} />
-					</li>
-				{/each}
-			{/if}
+			{#each $counters as counter, index}
+				<li>
+					<Counter {index} {clickedIndex} />
+				</li>
+			{/each}
 		</ul>
 	</div>
 </main>
