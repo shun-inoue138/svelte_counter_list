ffe15a29b550a54b7496b8121a3b625b7871b9d4
inoue <ht8sk13@icloud.com>
2023-01-29 12:33:15 +0900

    各countの合計とtitleの列挙を表示。$countersをeachで展開

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,38 @@
-<div class="bg-gray-500 text-white">hello world!</div>
+<script lang="ts">
+	import { counters } from '../store.js';
+	import Counter from '../components/Counter.svelte';
+	const addCounter = () => {
+		$counters = [...$counters, { title: 'new title', count: 0 }];
+	};
+</script>
+
+<main class="min-h-screen bg-gray-100">
+	<div class="w-[96%] mx-auto text-center">
+		<h1 class="text-4xl font-bold text-gray-900">Multiple Counter</h1>
+		<div class="text-3xl">
+			total: <span class="border-b border-black"
+				>{$counters.reduce((acc, cur) => acc + cur.count, 0)}</span
+			>
+		</div>
+		<ul class="flex flex-wrap gap-1 ">
+			<!-- todo:TitleListコンポーネント化 -->
+			{#each $counters as counter}
+				<li class="text-xs">
+					{counter.title}
+				</li>
+			{/each}
+		</ul>
+		<button on:click={addCounter}>new counter</button>
+		<ul>
+			{#if $counters === null}
+				<li>no counter</li>
+			{:else}
+				{#each $counters as counter, index}
+					<li>
+						<Counter {index} />
+					</li>
+				{/each}
+			{/if}
+		</ul>
+	</div>
+</main>
