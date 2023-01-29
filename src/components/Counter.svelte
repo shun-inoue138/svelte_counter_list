28a42862c2e0efbf790a5c347f28dfe1e8d0883e
inoue <ht8sk13@icloud.com>
2023-01-30 01:33:02 +0900

    icon読み込み

--- src/components/Counter.svelte
+++ src/components/Counter.svelte
@@ -4,6 +4,7 @@
 	export let index: number;
 	export let clickedIndex: number;
 	import { counters } from '../store.js';
+	import Icon from '@Iconify/svelte';
 	let inputEL = null;
 	$: if (index === clickedIndex) {
 		inputEL.focus();
@@ -25,11 +26,36 @@
 	};
 </script>
 
-<div in:fly={{ y: 30 }}>
-	<input type="text" bind:value={$counters[index].title} bind:this={inputEL} />
-	<span>{$counters[index].count}</span>
-	<button on:click={increment}>+</button>
-	<button on:click={decrement}>-</button>
-	<button on:click={reset}>reset</button>
-	<button on:click={deleteCounter}>delete</button>
+<div
+	in:fly={{ y: 30 }}
+	class="px-4 py-2 bg-slate-400 mb-2 w-2/3 mx-auto shadow-2xl rounded-md flex gap-3 items-center"
+>
+	<label>
+		<p class="text-left">タイトル</p>
+		<input
+			type="text"
+			bind:value={$counters[index].title}
+			bind:this={inputEL}
+			class="rounded-md py-2 px-4"
+			placeholder="titleを入力してください"
+		/>
+	</label>
+	<div>
+		<p>現在の数値</p>
+		<span class="text-white text-xl">{$counters[index].count}</span>
+	</div>
+	<div class="ml-auto flex items-center gap-1">
+		<button on:click={increment}>
+			<Icon icon="mdi:increment" width="50" height="50" color="blue" />
+		</button>
+		<button on:click={decrement}>
+			<Icon icon="mdi:decrement" width="50" height="50" color="red" />
+		</button>
+		<button on:click={reset} class="ml-4">
+			<Icon icon="carbon:reset" color="#232338" width="30" height="30" />
+		</button>
+		<button on:click={deleteCounter} class="ml-4">
+			<Icon icon="material-symbols:delete-rounded" color="#232338" width="30" height="30" />
+		</button>
+	</div>
 </div>
