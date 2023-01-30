0e8c18887724bd0173ab1ff5161448cb68b811dd
inoue <ht8sk13@icloud.com>
2023-01-30 12:08:41 +0900

    createEventDispatcherに型を付けた

--- src/components/TitleItem.svelte
+++ src/components/TitleItem.svelte
@@ -2,7 +2,12 @@
 	import { fly } from 'svelte/transition';
 	import { createEventDispatcher } from 'svelte';
 	export let index: number;
-	const dispatch = createEventDispatcher();
+	type FocusIndexEvent = {
+		focusIndex: {
+			index: number;
+		};
+	};
+	const dispatch = createEventDispatcher<FocusIndexEvent>();
 </script>
 
 <li in:fly={{ x: -20 }} class="text-xs bg-black rounded-md text-white py-1 px-3">
